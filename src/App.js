e17af846734c0078d9fd318539f59fc6e2efe527
David <ddavidr569@gmail.com>
2020-02-10 07:20:22 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Button, TextField, FormControl } from "@material-ui/core";
+
+import Header from "./components/header";
+
+import "./app.css";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: {}
+    };
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  handleSubmit() {
+    alert("submeteu");
+  }
+
+  fetchUsers() {
+    const users = { users: { name: "david", livro: "teste" } };
+    console.log(users);
+    this.setState(users);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <FormControl>
+          <form onSubmit={() => this.handleSubmit()}>
+            <TextField
+              required
+              id="standard-required"
+              label="Required"
+              defaultValue="Hello World"
+            />
+            <Button variant="contained" type="submit" color="primary">
+              submit
+            </Button>
+          </form>
+        </FormControl>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./index.css";
+
+const Header = () => (
+  <div className="header-container">
+    <p>Teste</p>
+  </div>
+);
+
+export default Header;
